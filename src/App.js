da5bb1e5771c980a8de01cfcc265351c9523f840
jose vaudagna <vaudagnajose@gmail.com>
2022-02-18 20:14:24 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import './App.css';
-
+import {NavBar} from './components/NavBar/NavBar'
 function App() {
   return (
     <div className="App">
-      hola mundo
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './NavBar.css'
+const NavBar = () => {
+  return (
+    <ul>
+        <li>muebles</li>
+        <li>mesa</li>
+        <li>sillon</li>
+        <li>lamparas</li>
+    </ul>
+  )
+}
+
+export  {NavBar}
